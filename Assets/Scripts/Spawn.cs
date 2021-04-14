0f5adef96cccdd99ca579f2442d0e0487e3b33a3
MichaelEhnot <43359524+MichaelEhnot@users.noreply.github.com>
2021-04-14 00:34:50 -0400

    working on throwing and spawn

---
+++ Assets/Scripts/Spawn.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawn : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject toSpawnHelper;
+
+    private static GameObject toSpawn;
+
+    private static GameObject obj;
+
+    private void Start()
+    {
+        obj = this.gameObject;
+        toSpawn = toSpawnHelper;
+    }
+
+    public static void Respawn()
+    {
+        Instantiate(toSpawn, obj.transform.position, obj.transform.rotation);
+        
+    }
+}
---
+++ Assets/Scripts/Swipe.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+//code taken from https://www.youtube.com/watch?v=7O9bAFyGvH8
+public class Swipe : MonoBehaviour
+{
+    private Vector2 startPos, endPos, diection;
+    private float touchTimeStart, touchTimeFinish, timeInterval;
+
+
+    [SerializeField]
+    private float throwForceXandY = 1f;
+
+    [SerializeField]
+    private float throwForceZ = 50f;
+
+    Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        if(Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+        {
+            touchTimeStart = Time.time;
+            startPos = Input.GetTouch(0).position;
+        }
+
+        if(Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Ended)
+        {
+            touchTimeFinish = Time.time;
+
+            timeInterval = touchTimeFinish - touchTimeStart;
+
+            endPos = Input.GetTouch(0).position;
+
+            diection = startPos - endPos;
+
+            rb.isKinematic = false;
+            rb.AddForce(-diection.x * throwForceXandY, -diection.y * throwForceXandY, throwForceZ / timeInterval);
+
+            Destroy(gameObject, 4f);
+
+            Spawn.Respawn();
+        }
+    }
+
+}
