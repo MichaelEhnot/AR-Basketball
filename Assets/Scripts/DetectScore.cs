9d8f56fb2750301fc8edadb6839860f1d40f17a5
MichaelEhnot <43359524+MichaelEhnot@users.noreply.github.com>
2021-04-20 16:18:23 -0400

    before importing speech to text

---
+++ Assets/Scripts/DetectScore.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class DetectScore : MonoBehaviour
+{
+    private int score;
+
+    private AudioSource audioSource;
+
+    private void Start()
+    {
+        score = 0;
+        audioSource = gameObject.GetComponent<AudioSource>();
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "ball")
+        {
+            score++;
+            audioSource.Play();
+        }
+    }
+
+
+}
